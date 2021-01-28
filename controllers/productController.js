66d443ed68da20c802e0d53b4014264ebf7dadab
Joanna Doudeva <57722445+Judeva@users.noreply.github.com>
2021-01-28 22:46:37 +0200

    Add validation Input

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,34 +3,26 @@ const productService = require('../services/productService')
 const { validateInput } = require('./productHelper');
 const router= Router();
 
-
-//Show all products on home;
-router.get('/', (req, res)=>{
+router.get('/', (req, res) => {
     let products = productService.getAll(req.query);
-    console.log(req.query);
-    res.render('home', {title: 'Browse', products});
+
+    res.render('home', {title: 'Browse', products });
 });
 
-//Show one product on details:id;
-router.get('/details/:productId', (req, res)=>{
-    console.log(req.params);
-    let product = productService.getOne(req.params.productId);
-    res.render('details', {title: `Details`, product});
+router.get('/create', (req, res) => {
+    res.render('create', {title: 'Create'});
 });
 
-//Get the create page ;
-router.get('/create', (req, res)=>{
-    res.render('create', {title: `Add cube`});
-})
-
-//Create cube via form
-router.post('/create', validateInput,(req, res)=>{
+router.post('/create', validateInput, (req, res) => {
     productService.create(req.body)
-    .then(()=> res.redirect('/products'))
-    .catch(()=> res.status(500).end())
-    
-    res.redirect('/products');
+        .then(() => res.redirect('/products'))
+        .catch(() => res.status(500).end())
 });
 
+router.get('/details/:productId', (req, res) => {
+    let product = productService.getOne(req.params.productId);
+    
+    res.render('details',{title: 'Product Details', product})
+});
 
 module.exports = router;
--- controllers/productHelper.js
+++ controllers/productHelper.js
@@ -1,10 +1,21 @@
-exports.validateInput = function(res, req, next){
+exports.validateInput = function(req, res, next){
     let isValid= true;
 
-    console.log(req.body);
-    
-    console.log(req.body);
+    let {name, description, imageUrl, difficultyLevel} = req.body;
 
+    if(name===''||name.length<2){
+        isValid =false;
+        console.error('You should enter a valid name');
+    }
+    if(description===''||description.length<2){
+        isValid =false;
+        console.error('You should enter a valid description');
+    }
+    if(imageUrl===''||imageUrl.length<10){
+        isValid =false;
+        console.error('You should enter a valid image URL');
+    }
+   
     if(isValid){
         next();
     }
